aab7bfbfcc7f591237317e7de9d7ce0ea0cd5d7f
Adragon <15483983+AdragonGit@user.noreply.gitee.com>
2025-03-18 09:38:59 +0000

    "WS63的WiFi STA+HTTP Get请求天气预报案例代码"
    Signed-off-by: Adragon <15483983+AdragonGit@user.noreply.gitee.com>

---
+++ vendor/developers/demo/wifi_http_demo/inc/weather.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Adragon
+ *
+ * Description: WiFi STA and HTTP Get to get weather forecasts. \n
+ *              This file is used to parse data in json format.
+ *              
+ *
+ * History: \n
+ * 2025-03-18, Create file. \n
+ */
+#ifndef WEATHER_H
+#define WEATHER_H
+
+#include <stdio.h>
+#include <string.h>
+
+
+const char* get_city_name(const char* cityId);
+const char* get_weather_description(const char* numtq);
+const char* get_wind_description(const char* numfl);
+const char* get_wind_direction_description(const char* numfx);
+void parse_weather_data(const char* json_data);
+
+
+#endif // WEATHER_H
