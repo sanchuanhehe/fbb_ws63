519cf49adc0dd3d09ffe25635339cb1b1dd9beed
Adragon <15483983+AdragonGit@user.noreply.gitee.com>
2025-04-25 08:29:04 +0000

    update vendor/developers/demo/wifi_http_demo/inc/weather.h.
    Signed-off-by: Adragon <15483983+AdragonGit@user.noreply.gitee.com>

--- vendor/developers/demo/wifi_http_demo/inc/weather.h
+++ vendor/developers/demo/wifi_http_demo/inc/weather.h
@@ -3,7 +3,7 @@
  *
  * Description: WiFi STA and HTTP Get to get weather forecasts. \n
  *              This file is used to parse data in json format.
- *              
+ *
  *
  * History: \n
  * 2025-03-18, Create file. \n
@@ -14,12 +14,10 @@
 #include <stdio.h>
 #include <string.h>
 
-
-const char* get_city_name(const char* cityId);
-const char* get_weather_description(const char* numtq);
-const char* get_wind_description(const char* numfl);
-const char* get_wind_direction_description(const char* numfx);
-void parse_weather_data(const char* json_data);
-
+const char *get_city_name(const char *cityId);
+const char *get_weather_description(const char *numtq);
+const char *get_wind_description(const char *numfl);
+const char *get_wind_direction_description(const char *numfx);
+void parse_weather_data(const char *json_data);
 
 #endif // WEATHER_H
