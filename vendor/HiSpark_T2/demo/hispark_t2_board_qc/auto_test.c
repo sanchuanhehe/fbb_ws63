157baf08544cb6d443be739935e67370c782e41c
唯教授 <2530308275@qq.com>
2025-04-28 16:47:37 +0800

    update auto_test.c

--- vendor/HiSpark_T2/demo/hispark_t2_board_qc/auto_test.c
+++ vendor/HiSpark_T2/demo/hispark_t2_board_qc/auto_test.c
@@ -1,6 +1,19 @@
-/**
-*    Copyright (c) 2024/12/18  KangBohao@OurEDA， Dalian Univ of Tech
-*/
+/*
+ * Copyright (c) 2024 HiSilicon Technologies CO., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * Copyright (c) 2024/12/18  KangBohao@OurEDA， Dalian Univ of Tech
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "pinctrl.h"
 #include "common_def.h"
