29eebee8649fb871d918852b13db2129570a71f4
Adragon <15483983+AdragonGit@user.noreply.gitee.com>
2025-05-28 09:11:21 +0000

    update vendor/developers/demo/UartInt_demo/src/main.c.
    Signed-off-by: Adragon <15483983+AdragonGit@user.noreply.gitee.com>

--- vendor/developers/demo/UartInt_demo/src/main.c
+++ vendor/developers/demo/UartInt_demo/src/main.c
@@ -20,6 +20,18 @@
 #define MSLEEP_TIME 5
 #define UART_SEND_CMD_WAITTIME 1000
 
+uint8_t resend0 = 0; 
+uint8_t resend1 = 1;
+uint8_t *exp_ok = (uint8_t *)"OK";
+uint8_t *exp_mqttopen = (uint8_t *)"+MQTTOPEN";
+static uint8_t *at_ate0 = (uint8_t *)"ATE0\r\n";
+static uint8_t *at_mipcall = (uint8_t *)"AT+MIPCALL=1\r\n";
+uint8_t *at_mqttuser = (uint8_t *)"AT+MQTTUSER=1,\"username\",\"userid\",\"password\"\r\n";
+uint8_t *at_mqttopen = (uint8_t *)"AT+MQTTOPEN=1,\"xxx.xxx.xx.xxx\",port,0,60\r\n";
+static uint8_t *at_mqttsub1 = (uint8_t *)"AT+MQTTSUB=1,\"v1/devices/me/rpc/request/+\",1\r\n";
+static uint8_t *at_mqttsub2 = (uint8_t *)"AT+MQTTSUB=1,\"v1/devices/me/attributes/response/+\",1\r\n";
+static uint8_t *at_mqttsub3 = (uint8_t *)"AT+MQTTSUB=1,\"v1/devices/me/attributes\",1\r\n";
+
 
 static void *Gateway_Task(void)
 {
@@ -56,20 +68,22 @@ static void *Gateway_Task(void)
     }
 
     // MQTT入网
-    UartInt_Send_Cmd("ATE0\r\n", "OK", 0);
-    UartInt_Send_Cmd("AT+MIPCALL=1\r\n", "OK", 1);
+    UartInt_Send_Cmd(at_ate0, exp_ok, resend0);
+    UartInt_Send_Cmd(at_mipcall, exp_ok, resend0);
     osal_msleep(UART_SEND_CMD_WAITTIME);
-    UartInt_Send_Cmd("AT+MQTTUSER=1,\"username\",\"userid\",\"password\"\r\n", "OK", 1);
-    UartInt_Send_Cmd("AT+MQTTOPEN=1,\"xxx.xxx.xx.xxx\",port,0,60\r\n", "+MQTTOPEN", 0);
-    UartInt_Send_Cmd("AT+MQTTSUB=1,\"v1/devices/me/rpc/request/+\",1\r\n", "OK", 0);
+    UartInt_Send_Cmd(at_mqttuser, exp_ok, resend1);
+    UartInt_Send_Cmd(at_mqttopen, exp_mqttopen, resend0);
+    UartInt_Send_Cmd(at_mqttsub1, exp_ok, resend0);
     osal_msleep(UART_SEND_CMD_WAITTIME);
-    UartInt_Send_Cmd("AT+MQTTSUB=1,\"v1/devices/me/rpc/request/+\",1\r\n", "OK", 0);
-    UartInt_Send_Cmd("AT+MQTTSUB=1,\"v1/devices/me/attributes/response/+\",1\r\n","OK", 0);
-    UartInt_Send_Cmd("AT+MQTTSUB=1,\"v1/devices/me/attributes\",1\r\n", "OK", 0);
+    UartInt_Send_Cmd(at_mqttsub1, exp_ok, resend0);
+    UartInt_Send_Cmd(at_mqttsub2, exp_ok, resend0);
+    UartInt_Send_Cmd(at_mqttsub3, exp_ok, resend0);
     osal_printk("*******************************************\r\n");
     osal_printk("Uart_MQTT_Test Init Done!\r\n");
     osal_printk("*******************************************\r\n");
     osal_msleep(UART_SEND_CMD_WAITTIME);
+    
+    return NULL;
 }
 
 void TaskEntry(void)
