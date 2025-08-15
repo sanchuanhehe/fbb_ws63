c39d314d8f60165c52351d66a873573c3e2b6bfa
sanchuanhehe <wyihe5220@gmail.com>
2025-08-15 14:10:19 +0800

    修正 activate_env.sh 中虚拟环境激活路径；新增 config.sh 脚本以自动安装系统依赖和 Python 包

--- src/activate_env.sh
+++ src/activate_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 激活虚拟环境
-source ../.venv/bin/activate
+source .venv/bin/activate
 export LC_ALL=C.UTF-8
 export PATH="/sdk/tools/bin/compiler/riscv/cc_riscv32_musl_b090/cc_riscv32_musl_fp/bin:${PATH}"
 echo "✓ 开发环境已激活"
